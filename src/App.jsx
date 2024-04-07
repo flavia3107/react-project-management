01da3037ec45ca9e0f4d074e99fafe1aa6124a7c
Flavia Kosti <flaviakosti@gmail.com>
2024-04-06 20:36:19 -0400

    Project-Management: Set up main components

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import NewProject from "./components/NewProject.jsx";
 import ProjectsSidebars from "./components/ProjectsSidebar.jsx";
+import NoProjectSelected from "./components/NoProjectSelected.jsx";
 
 function App() {
   return (
     <main className="h-screen my-8 flex gap-8">
      <ProjectsSidebars/>
-     <NewProject/>
+     <NoProjectSelected/>
     </main>
   );
 }
