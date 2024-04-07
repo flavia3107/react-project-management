bdff73213dd75f45f1ee47eeed5c42e0de6fb45c
Flavia Kosti <flaviakosti@gmail.com>
2024-04-06 20:58:49 -0400

    Project-Management: Show views on conditions

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,29 @@
 import NewProject from "./components/NewProject.jsx";
 import ProjectsSidebars from "./components/ProjectsSidebar.jsx";
 import NoProjectSelected from "./components/NoProjectSelected.jsx";
+import { useState } from 'react';
 
 function App() {
+  const [projectState, setProjectState] = useState({
+    selectedProjectId: undefined,
+    projects: []
+  });
+
+  function createNewProject(){
+   setProjectState(prevState => ({...prevState, selectedProjectId: null}));
+  }
+
+  let content;
+  if(projectState.selectedProjectId === null){
+    content = <NewProject/>
+  } else if (projectState.selectedProjectId === undefined){
+	content = <NoProjectSelected onCreateNewProject={createNewProject}/>
+  }
+
   return (
     <main className="h-screen my-8 flex gap-8">
-     <ProjectsSidebars/>
-     <NoProjectSelected/>
+     <ProjectsSidebars onCreateNewProject={createNewProject}/>
+	 {content}
     </main>
   );
 }
